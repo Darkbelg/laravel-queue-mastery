1f0c1aea21bc34d6a10d116329f4f22fe7567426
Darkbelg <stijn.sagaert@outlook.com>
2022-03-03 18:01:08 +0100

    Dispatching Workflows Episode 4

---
+++ app/Jobs/PullRepo.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class PullRepo implements ShouldQueue
+{
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $name;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if ($this->batch()->canceled()){
+            return;
+        }
+
+        info("Start Pulling Repo: " . $this->name);
+        sleep(rand(0,2));
+        info("Finish Pulling Repo: " . $this->name);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
     //\App\Jobs\SendWelcomeEmail::dispatch()->delay(5);
 
 //    foreach (range(1,100) as $i){
-        \App\Jobs\SendWelcomeEmail::dispatch();
+//        \App\Jobs\SendWelcomeEmail::dispatch();
 //    }
 
     // in order to retry a job
@@ -34,6 +34,28 @@ Route::get('/', function () {
     // Run a special que and with priority
     // php artisan queue:work --queue=payments,default
 //    \App\Jobs\ProcessPayment::dispatch()->onQueue('payments');
+
+    // A chain is a series of task done sequentially
+    // If one fails the whole job fails
+//    $chain = [
+//        new \App\Jobs\PullRepo(),
+//        new \App\Jobs\RunTests(),
+//        new \App\Jobs\Deploy(),
+//    ];
+//
+//    \Illuminate\Support\Facades\Bus::chain($chain)->dispatch();
+
+    // a batch is a series of jobs that can run parallel(Unless sleep command is influencing how it works this is not parallel
+    // When a job inside a batch fails the entire job will be cancelled
+    $batch = [
+        new \App\Jobs\PullRepo('ll1'),
+        new \App\Jobs\PullRepo('ll2'),
+        new \App\Jobs\PullRepo('ll3'),
+    ];
+
+    \Illuminate\Support\Facades\Bus::Batch($batch)
+        ->allowsFailures()
+        ->dispatch();
 
     return view('welcome');
 });
